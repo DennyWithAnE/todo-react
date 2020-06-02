690b3db24e13f14b0ca6c1d0b2e1ba58c6105b19
Denny Koh <denny.koh.kaichao@gmail.com>
2020-06-02 09:54:06 +0800

    initially cannot display the list but realised the mistake was the rendering part calling list instead of toDoList

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
+import React from "react";
+import { hot } from "react-hot-loader";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      list: "",
+      toDoList: [],
+    };
+  }
+  handleChange(event) {
+    this.setState({
+      list: event.target.value,
+    });
+  }
+  handleSubmit(event) {
+    var inputList = this.state.list;
+
+    if (inputList.length < 3 || inputList.length > 200) {
+      this.setState({
+        inputError: "Input should be between 3 and 200 characters!",
+      });
+    } else {
+      var inputList = this.state.toDoList;
+      inputList.push(this.state.list);
+      this.setState({ list: "" });
+    }
+  }
   render() {
+    var toDoList = this.state.toDoList.map((list) => {
+      return <li>{list}</li>;
+    });
     return (
-      <div>
-        Welcome.
+      <div className="item">
+        <button
+          onClick={(event) => {
+            this.handleSubmit();
+          }}
+        >
+          Add to list
+        </button>
+        <ul>{toDoList}</ul>
+        <p>{this.state.inputError}</p>
+        <input
+          className={this.state.inputClass}
+          onChange={(event) => {
+            this.handleChange(event);
+          }}
+          value={this.state.list}
+        />
       </div>
     );
   }
